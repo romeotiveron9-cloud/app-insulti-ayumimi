969444eba2466388a6b47ee99455431a48288d58
romeotiveron9-cloud <romeotiveron9@gmail.com>
2026-02-24 21:00:00 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='ayumi-v1';const urlsToCache=['index.html','style.css','script.js','manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
